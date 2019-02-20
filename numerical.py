47673a74ea5c8dae826de7b259d2bf48e8e70511
Grant Smith <grant.smith@tufts.edu>
2019-02-20 15:39:08 -0500

    Create numerical.py

---
+++ numerical.py
@@ -0,0 +1,19 @@
+import math
+
+def print_system(time,pos,vel,accl):
+    print("TIME:     ", time, ' s')
+    print("POSITION: ", pos , ' rads')
+    print("VELOCITY: ", vel, ' m/s')
+    print("ACCELERATION: ", accl, 'm/s^2', "\n")
+
+def time():
+
+
+def position(t):
+    mangle = (math.pi/12)
+    pos = mangle * math.sin(math.sqrt((9.8)/(.1))*t-(mathj.pi/2)
+
+def velocity(t):
+
+
+def acceleration():
