d8ec76a1618812c48483f539e85760f3591516d1
Grant Smith <grant.smith@tufts.edu>
2019-02-21 15:22:15 -0500

    Update numerical.py

--- numerical.py
+++ numerical.py
@@ -1,7 +1,7 @@
 import math
 
 def print_system(time,pos,vel,accl):
-    print("TIME:     ", time, ' s')
+    print("TIME:     ", round(time,2), ' s')
     print("POSITION: ", pos , ' degrees')
     print("VELOCITY: ", vel, ' m/s')
     print("ACCELERATION: ", accl, 'm/s^2', "\n")
@@ -11,7 +11,7 @@ def position(t):
     mangle = (math.pi/12)
     sr = (9.8)/(.1)
     shift = (math.pi)/(2)
-    sin = math.sqrt(sr)*10-shift
+    sin = math.sqrt(sr)*t-shift
     rads = mangle * math.sin(sin) #position in radians
     pos =rads*(180/math.pi) #position in degerees
     return pos
@@ -24,9 +24,26 @@ def velocity(t):
     
     #v total
     vel = math.sqrt((v_x)**2+(v_y)**2)
-    print(v_x, v_y, vel)
+    return vel 
 
-def acceleration():
-    pass
+def acceleration(t):
+    #x
+    a_x = 2205*math.sin(7*math.sqrt(2)*t)**2*math.sin(15*math.cos(7*math.sqrt(2)*t))+147*math.cos(7*math.sqrt(2)*t)*math.cos(15*math.cos(7*math.sqrt(2)*t))
+    #y
+    a_y = 147*math.cos(7*math.sqrt(2)*t)*math.sin(15*math.cos(7*math.sqrt(2)*t))-2205*math.sin(7*math.sqrt(2)*t)**2*math.cos(15*math.cos(7*math.sqrt(2)*t))
+
+    #a total
+    accl = math.sqrt((a_x)**2+(a_y)**2)
+    return accl
 
 
+
+def movement():
+    time = 0
+    time_end = 1
+    while time <= time_end:
+        print_system(time, position(time), velocity(time), acceleration(time))
+        time += .07 # <--------how large step forward in time
+
+
+movement()
