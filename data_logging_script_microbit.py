3b1b353a1e7114bd01074bd78d7e7817cb7ca527
Grant Smith <grant.smith@tufts.edu>
2019-03-04 15:43:33 -0500

    Update data_logging_script_microbit.py

--- data_logging_script_microbit.py
+++ data_logging_script_microbit.py
@@ -1,7 +1,15 @@
+#Logs accelerometer values of the microbit in the x, y, and z axies 
+#Created by: Grant Smith and Patrick Wright
+
 import microbit
 import math
+
+#global variables
 toggle = 0
 collectaccel = microbit.Image('00000:00000:09990:00000:00000')
+
+#script that collects data
+#press a to start data collection and press a again to end data collection
 while toggle == 0:
     if microbit.button_a.is_pressed() == True:
         toggle = 1
